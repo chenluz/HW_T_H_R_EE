883109b2eab62fe86c5d030be28e15368172e6c2
Siliang <lusiliang93@gmail.com>
2017-04-30 19:32:36 -0400

    remove unnecessary comments

--- deeprl_hw3/reinforce.py
+++ deeprl_hw3/reinforce.py
@@ -79,14 +79,14 @@ def reinforce(env):
     """
     return 0.0
 
-def discount_rewards(r):
+def total_rewards(r):
     """ take 1D float array of rewards and compute reward without discount"""
-    discounted_r = np.zeros_like(r)
+    total_r = np.zeros_like(r)
     running_add = 0
     for t in reversed(range(0, r.size)):
         running_add = running_add + r[t]
-        discounted_r[t] = running_add
-    return discounted_r
+        total_r[t] = running_add
+    return total_r
 
 def main():
 
@@ -97,14 +97,14 @@ def main():
     learning_rate = 1e-2 # feel free to play with this to train faster or more stably.
     gamma = 0.99 # discount factor for reward
 
-    D = 4 # input dimensionality
+    D = 4 # for Cartpole, input dimension is 4
     tf.reset_default_graph()
 
     #This defines the network as it goes from taking an observation of the environment to 
     #giving a probability of chosing to the action of moving left or right.
     observations = tf.placeholder(tf.float32, [None,D] , name="input_x")
     fc1 = tf.layers.dense(inputs=observations, units=16, activation=tf.nn.relu)
-    probability = tf.layers.dense(inputs=fc1, units=2, activation=tf.nn.sigmoid)
+    probability = tf.layers.dense(inputs=fc1, units=1, activation=tf.nn.sigmoid)
 
     #From here we define the parts of the network needed for learning a good policy.
     tvars = tf.trainable_variables()
@@ -115,29 +115,28 @@ def main():
 
     # The loss function. This sends the weights in the direction of making actions 
     # that gave good advantage (reward over time) more likely, and actions that didn't less likely.   
-    loglik = tf.log(input_y*(input_y - probability) + (1 - input_y)*(input_y + probability))
-    loss = -tf.reduce_mean(loglik * advantages) 
+    logp = tf.log(input_y*(input_y - probability) + (1 - input_y)*(input_y + probability))
+    loss = -tf.reduce_mean(logp * advantages) 
 
     # Once we have collected a series of gradients from multiple episodes, we apply them.
     # We don't just apply gradeients after every episode in order to account for noise in the reward signal.
     adam = tf.train.AdamOptimizer(learning_rate=learning_rate) # Our optimizer
-    W1Grad = tf.placeholder(tf.float32,name="batch_grad1") # Placeholders to send the final gradients through when we update.
-    W2Grad = tf.placeholder(tf.float32,name="batch_grad2")
+    Grad1 = tf.placeholder(tf.float32,name="batch_grad1") # Placeholders to send the final gradients through when we update.
+    Grad2 = tf.placeholder(tf.float32,name="batch_grad2")
 
-    batchGrad = [W1Grad,W2Grad]
+    batchGrad = [Grad1,Grad2]
     newGrads = adam.compute_gradients(loss,tvars)
     updateGrads = adam.apply_gradients(zip(batchGrad,tvars))
 
-    xs,hs,dlogps,drs,ys,tfps,ps = [],[],[],[],[],[],[]
-    running_reward = None
+    xs,hs,dlogps,drs,ys, = [],[],[],[],[]
     reward_sum = 0
     episode_number = 1
-    total_episodes = 1000
+    total_episodes = 10000
     init = tf.global_variables_initializer()
 
     # Launch the graph
     with tf.Session() as sess:
-        rendering = False
+        flag = True
         sess.run(init)
         observation = env.reset() # Obtain an initial observation of the environment
 
@@ -151,9 +150,8 @@ def main():
 
             # Rendering the environment slows things down, 
             # so let's only look at it once our agent is doing a good job.
-            if reward_sum/batch_size > 100 or rendering == True : 
-               # env.render()
-                rendering = True
+            if reward_sum/batch_size > 100 or flag == True : 
+                flag = True
             
             # Make sure the observation is in a shape the network can handle.
             x = np.reshape(observation,[1,D])
@@ -180,40 +178,34 @@ def main():
                 # stack together all inputs, hidden states, action gradients, and rewards for this episode
                 epx = np.vstack(xs)
                 epy = np.vstack(ys)
-                epr = np.vstack(drs)
+                epdr = np.vstack(drs)
 
-                tfp = tfps
-                xs,hs,dlogps,drs,ys,tfps,ps = [],[],[],[],[],[],[] # reset array memory
+                xs,hs,dlogps,drs,ys = [],[],[],[],[] # reset array memory
 
                 # compute the discounted reward backwards through time
-                discounted_epr = discount_rewards(epr)
+                total_epdr = total_rewards(epdr)
                 # size the rewards to be unit normal (helps control the gradient estimator variance)
-                discounted_epr -= np.mean(discounted_epr)
-                discounted_epr /= np.std(discounted_epr)
+                total_epdr -= np.mean(total_epdr)
+                total_epdr /= np.std(total_epdr)
 
                 # Get the gradient for this episode, and save it in the gradBuffer
-                tGrad = sess.run(newGrads,feed_dict={observations: epx, input_y: epy, advantages: discounted_epr})
+                tGrad = sess.run(newGrads,feed_dict={observations: epx, input_y: epy, advantages: total_epdr})
                 tGrad = [x[0] for x in tGrad]
                 for ix,grad in enumerate(tGrad):
                     gradBuffer[ix] += grad
                 
                 # If we have completed enough episodes, then update the policy network with our gradients.
                 if episode_number % batch_size == 0: 
-                    sess.run(updateGrads,feed_dict={W1Grad: gradBuffer[0],W2Grad:gradBuffer[1]}) 
+                    sess.run(updateGrads,feed_dict={Grad1: gradBuffer[0],Grad2:gradBuffer[1]}) 
                     for ix,grad in enumerate(gradBuffer):
                         gradBuffer[ix] = grad * 0
 
                     # Give a summary of how well our network is doing for each batch of episodes.
-                    running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
                     print ('Average reward for episode {}'.format(reward_sum/batch_size))
-                    print ('Total average reward {}'.format(running_reward/batch_size))
 
-                    if reward_sum/batch_size > 200: 
-                        print('Task solved in'+ str(episode_number) + 'episodes!')
-                        break
                     reward_sum = 0
 
-                if rendering == True and episode_number % 100 == 0:
+                if flag == True and episode_number % 1000 == 0:
                     f1 = open('test_{}.txt'.format(episode_number),'w')
                     episode_length = 0
                     observation_eval = env.reset()
